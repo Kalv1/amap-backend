0f54911831c9d434b0f25195d1313bc761aacce2
Eisenpic <eisenpic@hotmail.fr>
2022-02-22 14:42:41 +0100

    :sparkles: RecetteSeeder, fill db of recipes

--- app/Models/Recette.php
+++ app/Models/Recette.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Recette extends Model
 {
+    use hasFactory;
+
     protected $fillable = [
         'titre',
         'description',
